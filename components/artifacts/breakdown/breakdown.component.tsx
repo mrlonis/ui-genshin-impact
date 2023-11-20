05425115dccdd82ffab0686b582fec38a8662e00
Matthew Lonis <mattlonis@gmail.com>
2023-11-19 21:57:49 -0500

    Enhanced artifact breakdown screen

--- components/artifacts/breakdown/breakdown.component.tsx
+++ components/artifacts/breakdown/breakdown.component.tsx
@@ -1,48 +1,215 @@
 'use client'
 
-import { ArtifactBreakdown, ArtifactBreakdownMap } from './artifact-breakdown'
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Image,
+  Listbox,
+  ListboxItem,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from '@nextui-org/react'
+import React from 'react'
+import { ArtifactBreakdown, ArtifactBreakdownCharacter, ArtifactBreakdownMap } from './artifact-breakdown'
 
-function createArrayString(artifactBreakdownMap: ArtifactBreakdownMap | null | undefined): string {
+interface TableData {
+  [key: string]: string | number | ArtifactBreakdownCharacter[]
+  id: number
+  stat: string
+  characters: ArtifactBreakdownCharacter[]
+}
+
+function createTableData(artifactBreakdownMap: ArtifactBreakdownMap | null | undefined): TableData[] {
   if (!artifactBreakdownMap) {
-    return 'null'
+    return []
   }
-  let x = 0
-  let returnValue = ''
+  let returnValue: TableData[] = []
+  let i = 0
   for (const [key, value] of Object.entries(artifactBreakdownMap)) {
-    if (x != 0) {
-      returnValue += ' / '
-    }
-    returnValue += `${key}: (`
-    let i = 0
+    let characters: ArtifactBreakdownCharacter[] = []
     for (let character of value) {
-      if (i != 0) {
-        returnValue += ', '
-      }
-      returnValue += `${character.name}`
-      i += 1
+      characters.push(character)
     }
-    returnValue += ')'
-    x += 1
+    returnValue.push({ id: i, stat: key, characters: characters })
+    i += 1
   }
   return returnValue
 }
 
+const columns = [
+  { name: 'STAT', uid: 'stat' },
+  { name: 'CHARACTERS', uid: 'characters' },
+]
+
 export default function ArtifactBreakdownComponent(props: { artifactBreakdown: ArtifactBreakdown }) {
   // URL -> `/dashboard?search=my-project`
   // `search` -> 'my-project'
+  const renderCell = React.useCallback((item: TableData, columnKey: string | number) => {
+    console.log('renderCell')
+    console.log(item)
+    console.log(columnKey)
+    const cellValue = item[columnKey]
+
+    switch (columnKey) {
+      case 'stat':
+        let typedValue = cellValue as string
+        return <p>{typedValue}</p>
+      case 'characters':
+        let typedValueCharacters = cellValue as ArtifactBreakdownCharacter[]
+        return (
+          <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
+            <Listbox items={typedValueCharacters} aria-label="Dynamic Actions" onAction={(key) => alert(key)}>
+              {(item) => (
+                <ListboxItem
+                  key={item.id}
+                  color={item.name === 'delete' ? 'danger' : 'default'}
+                  className={item.name === 'delete' ? 'text-danger' : ''}
+                >
+                  {item.name}
+                </ListboxItem>
+              )}
+            </Listbox>
+          </div>
+        )
+      default:
+        return <p>Default</p>
+    }
+  }, [])
+
   return (
-    <div>
-      <p>Breakdown</p>
-      <p>ID: {props.artifactBreakdown.id}</p>
-      <p>Name: {props.artifactBreakdown.name}</p>
-      <p>Image URL: {props.artifactBreakdown.imageUrl ?? 'null'}</p>
-      <p>1-Piece: {props.artifactBreakdown.onePieceSetEffect ?? 'null'}</p>
-      <p>2-Piece: {props.artifactBreakdown.twoPieceSetEffect ?? 'null'}</p>
-      <p>4-Piece: {props.artifactBreakdown.fourPieceSetEffect ?? 'null'}</p>
-      <p>Sands Stats: {createArrayString(props.artifactBreakdown.sandsStats)}</p>
-      <p>Goblet Stats: {createArrayString(props.artifactBreakdown.gobletStats)}</p>
-      <p>Circlet Stats: {createArrayString(props.artifactBreakdown.circletStats)}</p>
-      <p>Subtats: {createArrayString(props.artifactBreakdown.substats)}</p>
-    </div>
+    <Card className="max-w-[100%]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="artifact logo"
+          height={40}
+          radius="sm"
+          src={props.artifactBreakdown.imageUrl ?? 'https://avatars.githubusercontent.com/u/86160567?s=200&v=4'}
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">{props.artifactBreakdown.name}</p>
+        </div>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <p>1-Piece: {props.artifactBreakdown.onePieceSetEffect ?? 'null'}</p>
+        <p>2-Piece: {props.artifactBreakdown.twoPieceSetEffect ?? 'null'}</p>
+        <p>4-Piece: {props.artifactBreakdown.fourPieceSetEffect ?? 'null'}</p>
+      </CardBody>
+      <Divider className="my-4" />
+      <CardFooter>
+        <Table isStriped aria-label="Example static collection table">
+          <TableHeader>
+            <TableColumn>STAT TYPE</TableColumn>
+            <TableColumn>BREAKDOWN</TableColumn>
+          </TableHeader>
+          <TableBody emptyContent={'No rows to display.'}>
+            <TableRow key="1">
+              <TableCell>Sands Stats</TableCell>
+              <TableCell>
+                <Table aria-label="Example table with custom cells">
+                  <TableHeader columns={columns}>
+                    {(column) => (
+                      <TableColumn key={column.uid} align={column.uid === 'actions' ? 'center' : 'start'}>
+                        {column.name}
+                      </TableColumn>
+                    )}
+                  </TableHeader>
+                  <TableBody
+                    items={createTableData(props.artifactBreakdown.sandsStats)}
+                    emptyContent={'No rows to display.'}
+                  >
+                    {(item) => (
+                      <TableRow key={item.id}>
+                        {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </TableCell>
+            </TableRow>
+            <TableRow key="2">
+              <TableCell>Goblet Stats</TableCell>
+              <TableCell>
+                <Table aria-label="Example table with custom cells">
+                  <TableHeader columns={columns}>
+                    {(column) => (
+                      <TableColumn key={column.uid} align={column.uid === 'actions' ? 'center' : 'start'}>
+                        {column.name}
+                      </TableColumn>
+                    )}
+                  </TableHeader>
+                  <TableBody
+                    items={createTableData(props.artifactBreakdown.gobletStats)}
+                    emptyContent={'No rows to display.'}
+                  >
+                    {(item) => (
+                      <TableRow key={item.id}>
+                        {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </TableCell>
+            </TableRow>
+            <TableRow key="3">
+              <TableCell>Circlet Stats</TableCell>
+              <TableCell>
+                <Table aria-label="Example table with custom cells">
+                  <TableHeader columns={columns}>
+                    {(column) => (
+                      <TableColumn key={column.uid} align={column.uid === 'actions' ? 'center' : 'start'}>
+                        {column.name}
+                      </TableColumn>
+                    )}
+                  </TableHeader>
+                  <TableBody
+                    items={createTableData(props.artifactBreakdown.circletStats)}
+                    emptyContent={'No rows to display.'}
+                  >
+                    {(item) => (
+                      <TableRow key={item.id}>
+                        {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </TableCell>
+            </TableRow>
+            <TableRow key="4">
+              <TableCell>Subtats</TableCell>
+              <TableCell>
+                <Table aria-label="Example table with custom cells">
+                  <TableHeader columns={columns}>
+                    {(column) => (
+                      <TableColumn key={column.uid} align={column.uid === 'actions' ? 'center' : 'start'}>
+                        {column.name}
+                      </TableColumn>
+                    )}
+                  </TableHeader>
+                  <TableBody
+                    items={createTableData(props.artifactBreakdown.substats)}
+                    emptyContent={'No rows to display.'}
+                  >
+                    {(item) => (
+                      <TableRow key={item.id}>
+                        {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardFooter>
+    </Card>
   )
 }
--- components/stacked-layout.component.tsx
+++ components/stacked-layout.component.tsx
@@ -45,9 +45,19 @@ function determineIsActive(pathname: string, item: INavigation) {
   return pathname.startsWith(item.href)
 }
 
+function determineHeaderText(pathname: string) {
+  for (let item of navigation) {
+    if (determineIsActive(pathname, item)) {
+      return item.name
+    }
+  }
+  return 'Dashboard'
+}
+
 export default function StackedLayout(props: PropsWithChildren) {
   const pathname = usePathname()
   console.log(`StackedLayout: pathname = ${pathname}`)
+  let header = determineHeaderText(pathname)
 
   return (
     <NextUIProvider>
@@ -221,7 +231,7 @@ export default function StackedLayout(props: PropsWithChildren) {
 
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">{header}</h1>
           </div>
         </header>
         <main>
