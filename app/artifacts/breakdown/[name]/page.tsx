4c8b7d81812eb99c564cfd07e66d35bf5945be7e
Matthew Lonis <mattlonis@gmail.com>
2023-11-26 20:48:17 -0500

    Removing unused code

--- app/artifacts/breakdown/[name]/page.tsx
+++ app/artifacts/breakdown/[name]/page.tsx
@@ -1,5 +1,4 @@
 import { ArtifactsResponse } from '@/components/artifacts/artifacts-response'
-import { ArtifactBreakdown } from '@/components/artifacts/breakdown/artifact-breakdown.model'
 import ArtifactBreakdownComponent from '@/components/artifacts/breakdown/breakdown.component'
 
 // TODO - Eventually we'll want to remove this when the backend is deployed somewhere
@@ -18,28 +17,8 @@ async function getArtifact(name: string): Promise<ArtifactsResponse> {
   return res.json()
 }
 
-async function getArtifactBreakdown(artifactId: string): Promise<ArtifactBreakdown> {
-  const res = await fetch(`http://localhost:9002/api/v2/artifactBreakdown?artifactId=${artifactId}&artifactDepth=5`, {
-    next: { revalidate: 1 },
-  })
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    console.error('getArtifactBreakdown(): Failed to fetch data')
-    console.error(res)
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
-
-  return res.json()
-}
-
-const artifactDepths = [1, 2, 3, 4, 5]
-
 export default async function ArtifactBreakdown({ params }: { params: { name: string } }) {
   const artifact = await getArtifact(params.name)
-  const artifactBreakdown = await getArtifactBreakdown(artifact.id)
 
   return (
     <div>
